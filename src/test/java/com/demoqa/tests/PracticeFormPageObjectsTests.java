36cfed7e1b3ef1450ac0f26b977b5f8e3f08df4a
LongSurfer <ivanov2avi@yandex.ru>
2023-04-23 20:14:09 +0300

    start code refactoring

--- src/test/java/com/demoqa/tests/PracticeFormPageObjectsTests.java
+++ src/test/java/com/demoqa/tests/PracticeFormPageObjectsTests.java
@@ -6,26 +6,18 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class PracticeFormTest extends TestBase {
-
+public class PracticeFormPageObjectsTests extends TestBase{
 
     @Test
     void successfulFillFormTest() {
-        open("/automation-practice-form");
 
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
-
-        $("#firstName").setValue("Alex");
-        $("#lastName").setValue("Evans");
-        $("#userEmail").setValue("test@test.qa");
-        $("#gender-radio-1").parent().click();
-        $("#userNumber").setValue("0123456789");
-
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").$(byText("November")).click();
-        $(".react-datepicker__year-select").$(byText("1990")).click();
-        $(".react-datepicker__day--025").click();
+        registrationPage.openPage()
+                        .setFirstName("Alex")
+                        .setLastName("Evans")
+                        .setUserEmail("test@test.qa")
+                        .setGender("Male")
+                        .setUserNumber("0123456789")
+                        .setBirthDate("25", "November", "1990");
 
         $("#subjectsInput").setValue("Eng");
         $("#react-select-2-option-0").click(); //кликаем на результат
--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,15 +1,22 @@
-package com.demoqa;
+package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.demoqa.pages.RegistrationPage;
+import com.demoqa.pages.components.CalendarComponent;
 import org.junit.jupiter.api.BeforeAll;
 
 
 public class TestBase {
 
+    RegistrationPage registrationPage = new RegistrationPage();
+
+
+
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
 //        Configuration.holdBrowserOpen = true;
 
     }
