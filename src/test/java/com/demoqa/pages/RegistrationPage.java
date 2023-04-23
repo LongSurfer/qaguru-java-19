958c6ac96a07233a8a3853d82635cb66358d8103
LongSurfer <ivanov2avi@yandex.ru>
2023-04-23 20:13:16 +0300

    added RegistrationPage.java

---
+++ src/test/java/com/demoqa/pages/RegistrationPage.java
@@ -0,0 +1,67 @@
+package com.demoqa.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import com.demoqa.pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+
+    // Selenide elements / locators / etc
+    SelenideElement firstNameInput = $("#firstName"),
+    lastNameInput = $("#lastName"),
+    userEmailInput = $("#userEmail"),
+    gender = $("#genterWrapper"),
+    userNumberInput = $("#userNumber"),
+    dateOfBirthInput = $("#dateOfBirthInput");
+
+
+    // Actions
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setUserEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        gender.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+
+        return this;
+    }
+
+    public void setBirthDate(String day, String month, String year) {
+        dateOfBirthInput.click();
+        calendarComponent.setDate(day, month, year);
+
+    }
+}
