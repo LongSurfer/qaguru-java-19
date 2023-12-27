a32e143f0c8ad8d332e6e80797aef3c78848b991
LongSurfer <ivanov2avi@yandex.ru>
2023-12-27 17:15:24 +0300

    add allure step

--- src/test/java/com/demoqa/tests/PracticeFormDataFakerTests.java
+++ src/test/java/com/demoqa/tests/PracticeFormDataFakerTests.java
@@ -2,7 +2,10 @@ package com.demoqa.tests;
 
 import com.demoqa.data.viewModel.RegistrationPageViewModel;
 import com.demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 public class PracticeFormDataFakerTests extends TestBase{
 
@@ -10,28 +13,33 @@ public class PracticeFormDataFakerTests extends TestBase{
     RegistrationPageViewModel registrationPageViewModel = new RegistrationPageViewModel();
 
     @Test
+    @DisplayName("Successful registration")
     void successfulFillFormTest() {
 
-        registrationPage.openPage()
-                        .closeAdBanners()
-                        .setFirstName(registrationPageViewModel.nameValue)
-                        .setLastName(registrationPageViewModel.lastNameValue)
-                        .setUserEmail(registrationPageViewModel.emailValue)
-                        .setGender(registrationPageViewModel.genderValue)
-                        .setUserNumber(registrationPageViewModel.numberValue)
-                        .setBirthDate(registrationPageViewModel.dayValue,
-                                registrationPageViewModel.monthValue,
-                                registrationPageViewModel.yearValue)
-                        .setSubject(registrationPageViewModel.subjectValue)
-                        .setHobby(registrationPageViewModel.hobbyValue)
-                        .selectPicture(registrationPageViewModel.pathValue)
-                        .setCurrentAddress(registrationPageViewModel.addressValue)
-                        .setStateAndCity(registrationPageViewModel.stateValue,
-                                registrationPageViewModel.cityValue)
-                        .clickOnSubmitButton();
+        step("Fill form", () -> {
+                    registrationPage.openPage()
+                            .closeAdBanners()
+                            .setFirstName(registrationPageViewModel.nameValue)
+                            .setLastName(registrationPageViewModel.lastNameValue)
+                            .setUserEmail(registrationPageViewModel.emailValue)
+                            .setGender(registrationPageViewModel.genderValue)
+                            .setUserNumber(registrationPageViewModel.numberValue)
+                            .setBirthDate(registrationPageViewModel.dayValue,
+                                    registrationPageViewModel.monthValue,
+                                    registrationPageViewModel.yearValue)
+                            .setSubject(registrationPageViewModel.subjectValue)
+                            .setHobby(registrationPageViewModel.hobbyValue)
+                            .selectPicture(registrationPageViewModel.pathValue)
+                            .setCurrentAddress(registrationPageViewModel.addressValue)
+                            .setStateAndCity(registrationPageViewModel.stateValue,
+                                    registrationPageViewModel.cityValue)
+                            .clickOnSubmitButton();
+                });
 
-        registrationPage.verifyRegistrationResultModalAppears()
-                .verifyResult(registrationPageViewModel);
+        step("Verify result", () -> {
+            registrationPage.verifyRegistrationResultModalAppears()
+                    .verifyResult(registrationPageViewModel);
+        });
 
     }
 }
